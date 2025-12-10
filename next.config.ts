2eb71007e13680a82c257ef078bc31b4651d77e9
thanhbinh2304 <binh2304tto@gmail.com>
2025-12-10 23:28:26 +0700

    add upload file img

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,28 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '127.0.0.1',
+        port: '8443',
+        pathname: '/storage/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '127.0.0.1',
+        port: '8000',
+        pathname: '/storage/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '8000',
+        pathname: '/storage/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
